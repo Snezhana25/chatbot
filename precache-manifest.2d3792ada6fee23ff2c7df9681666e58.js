e147f88cfad22a5f3fa722a9b4c451ff0f9bca95
Snezhana25 <snezhana.yarovaya@gmail.com>
2020-09-21 16:06:00 +0300

    Updates

---
+++ precache-manifest.2d3792ada6fee23ff2c7df9681666e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9c9e495451cb19271aacf38d001a92",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "5b9c14f2f5505d1354d1",
+    "url": "/chatbot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b00d594d2f9b6c20ae6",
+    "url": "/chatbot/static/js/2.8f48d322.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatbot/static/js/2.8f48d322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9c14f2f5505d1354d1",
+    "url": "/chatbot/static/js/main.91b97d17.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chatbot/static/media/logo.5d5d9eef.svg"
+  }
+]);
